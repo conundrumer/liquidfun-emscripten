2ba696128f505d5f5ec024ef196d72f4818f7b20
David Lu <davidelu93@gmail.com>
2018-10-05 16:18:49 -0400

    make exported vars constant + remove dup declaration

--- build-es6.sh
+++ build-es6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat liquidfun.js
+sed -e 's/var b2_/const b2_/g' -e 's/var _malloc=//' liquidfun.js
 echo "export {"
 grep -Po "var b2_\K[a-zA-Z0-9_.-]+" liquidfun.js | awk '{print "b2_" $1 " as " $1 ","}'
 grep -Po "function b2\K[a-zA-Z0-9_.-]+" liquidfun.js | awk '{print "b2" $1 " as " $1 ","}'
